c5a315063e806a651aeb2c5f9d6339ef9f0c0d8d
Marto Vesheff <martin.veshev@gmail.com>
2016-12-12 01:34:52 +0200

    Updated webpack demos - working

--- Topics/06. Ng-Modules/demos/modules-visibility/config/webpack.common.js
+++ Topics/06. Ng-Modules/demos/modules-visibility/config/webpack.common.js
@@ -4,53 +4,47 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var helpers = require('./helpers');
 
 module.exports = {
-  entry: {
-    'polyfills': './polyfills.ts',
-    'vendor': './vendor.ts',
-    'app': './main.ts'
-  },
+	entry: {
+		'polyfills': './polyfills.ts',
+		'vendor': './vendor.ts',
+		'app': './main.ts'
+	},
 
-  resolve: {
-    extensions: ['', '.ts', '.js']
-  },
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
 
-  module: {
-    loaders: [
-      {
-        test: /\.ts$/,
-        loaders: [
-			'awesome-typescript-loader',
-			 'angular2-template-loader'
-			 ]
-      },
-      {
-        test: /\.html$/,
-        loader: 'html'
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file?name=assets/[name].[hash].[ext]'
-      },
-      {
-        test: /\.css$/,
-        exclude: helpers.root('src', 'app'),
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
-      },
-      {
-        test: /\.css$/,
-        include: helpers.root('src', 'app'),
-        loader: 'raw'
-      }
-    ]
-  },
+	module: {
+		loaders: [{
+			test: /\.ts$/,
+			loaders: [
+				'awesome-typescript-loader',
+				'angular2-template-loader'
+			]
+		}, {
+			test: /\.html$/,
+			loader: 'html-loader'
+		}, {
+			test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+			loader: 'file?name=assets/[name].[hash].[ext]'
+		}, {
+			test: /\.css$/,
+			exclude: helpers.root('src', 'app'),
+			loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+		}, {
+			test: /\.css$/,
+			include: helpers.root('src', 'app'),
+			loader: 'raw-loader'
+		}]
+	},
 
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'vendor', 'polyfills']
-    }),
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: ['app', 'vendor', 'polyfills']
+		}),
 
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    })
-  ]
+		new HtmlWebpackPlugin({
+			template: './index.html'
+		})
+	]
 };
--- Topics/06. Ng-Modules/demos/modules-visibility/config/webpack.dev.js
+++ Topics/06. Ng-Modules/demos/modules-visibility/config/webpack.dev.js
@@ -4,21 +4,17 @@ var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
-  devtool: 'cheap-module-eval-source-map',
+	devtool: 'cheap-module-eval-source-map',
 
-  output: {
-    path: helpers.root('dist'),
-    publicPath: 'http://localhost:8080/',
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js'
-  },
+	output: {
+		path: helpers.root('dist'),
+		publicPath: 'http://localhost:8080/',
+		filename: '[name].js',
+		chunkFilename: '[id].chunk.js'
+	},
 
-  plugins: [
-    new ExtractTextPlugin('[name].css')
-  ],
-
-  devServer: {
-    historyApiFallback: true,
-    stats: 'minimal'
-  }
+	devServer: {
+		historyApiFallback: true,
+		stats: 'minimal'
+	}
 });
